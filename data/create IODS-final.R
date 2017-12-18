91e160e5c11c557fdf1140d9ac8bf7bdc7f5bc52
ElsaMarques <elsaoliveiramarques@gmail.com>
2017-12-18 22:48:10 +0200

    final changes

--- data/create IODS-final.R
+++ data/create IODS-final.R
@@ -13,13 +13,11 @@ data("hobbies")
 #Explore the data by exploring summary, dimension and head of the data 
 library(dplyr)
 summary(hobbies)
-
 dim(hobbies)
-
 head(hobbies)
+
   
 #You can see that some of the variables names are long
-
 #Rename the variables in order to facilitate the analysis of the data
 hobbies_ <- hobbies
 colnames(hobbies_) <- c("Read", "L.Music", "Cinema", "Show", "Exhib" , "Comp", "Sport", "Walk" , "Travel", "Play.M", "Collect", "Volunteer", "Mech", "Garden", "Knitt", "Cook", "Fish", "TV", "Gender","Age", "Marital.St", "Prof", "N.Act")
@@ -31,15 +29,12 @@ hobbies_[, "N.Act"] = as.factor(hobbies_[,"N.Act"])
 hobbies_
 head(hobbies_)
 
-#Remove all the data that is nor complete
-#Meaning remove all the 1498 NA's and respective data row
-
-# Filter out all rows with NA values
+# Remove all the data that is nor complete
+# Meaning remove all the 1498 NA's and respective data row
 hobbies_ <- na.omit(hobbies_)
 hobbies_
 
 # Make subsets of data according to the type of data
-
 # Define the variable Generic_Hobbies that will include c("Read", "L.Music", "Cinema", "Comp", "TV", "Gender","Age", "Marital.St", "Prof")
 Generic_Hobbies <- dplyr::select(hobbies_, c("Read", "L.Music", "Cinema", "Comp", "TV", "Gender","Age", "Marital.St", "Prof"))
 
@@ -49,10 +44,10 @@ Artistic_Hobbies <- dplyr::select(hobbies_, c("Show", "Exhib", "Play.M", "Collec
 # Define the variable Sport_Hobbies that will include c("Sport", "Walk" , "Travel", "Fish", "Gender","Age", "Marital.St", "Prof")
 Sport_Hobbies <- dplyr::select(hobbies_, c("Sport", "Walk" , "Travel", "Fish", "Gender","Age", "Marital.St", "Prof"))
 
-# Define the variable Handcraft_Hobbies that will include c("Mech", "Garden", "Knitt", "Cook")
+# Define the variable Handcraft_Hobbies that will include c("Mech", "Garden", "Knitt", "Cook", "Gender","Age", "Marital.St", "Prof")
 Handcraft_Hobbies <- dplyr::select(hobbies_, c("Mech", "Garden", "Knitt", "Cook", "Gender","Age", "Marital.St", "Prof"))
 
-# Define the variable Charity_Hobbies that will include c("Volunteer")
+# Define the variable Charity_Hobbies that will include c("Volunteer", "Gender","Age", "Marital.St", "Prof")
 Charity_Hobbies <- dplyr::select(hobbies_, c("Volunteer", "Gender", "Age", "Marital.St", "Prof"))
 
 #This new categories include 18 out of the 23 variables;
@@ -61,12 +56,13 @@ Charity_Hobbies <- dplyr::select(hobbies_, c("Volunteer", "Gender", "Age", "Mari
 
 
 #save main modified data as .CSV
-write.csv(hobbies_, file = "hobbies_.csv" )
+write.csv(hobbies_, file = "hobbies_.csv", row.names = FALSE)
+
 
 #save the subcategories / selected set as .CSV
-write.csv(Generic_Hobbies, file = "Generic_Hobbies.csv")
-write.csv(Artistic_Hobbies, file = "Artistic_Hobbies.csv")
-write.csv(Sport_Hobbies, file = "Sport_Hobbies.csv")
-write.csv(Handcraft_Hobbies, file = "Handcraft_Hobbies.csv")
-write.csv(Charity_Hobbies, file = "Charity_Hobbies.csv")
+write.csv(Generic_Hobbies, file = "Generic_Hobbies.csv", row.names = FALSE)
+write.csv(Artistic_Hobbies, file = "Artistic_Hobbies.csv", row.names = FALSE)
+write.csv(Sport_Hobbies, file = "Sport_Hobbies.csv", row.names = FALSE)
+write.csv(Handcraft_Hobbies, file = "Handcraft_Hobbies.csv", row.names = FALSE)
+write.csv(Charity_Hobbies, file = "Charity_Hobbies.csv", row.names = FALSE)
 
