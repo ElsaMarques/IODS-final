e0e807c02609efe38246a6a013b9d2ec50c677f4
ElsaMarques <elsaoliveiramarques@gmail.com>
2017-12-18 12:20:02 +0200

    final assignment works

--- data/create IODS-final.R
+++ data/create IODS-final.R
@@ -4,7 +4,7 @@
 #elsa.marques@helsinki.fi
 
 #Define the working directory
-setwd()
+setwd("~/Documents/GitHub/IODS-final/data")
 
 #Load data set Da
 library(FactoMineR)
@@ -30,34 +30,37 @@ colnames(hobbies_) <- c("Read", "L.Music", "Cinema", "Show", "Exhib" , "Comp", "
 #nb.activities is transformed to factors 
 hobbies_[, "N.Act"] = as.factor(hobbies_[,"N.Act"])
 hobbies_
-
+head(hobbies_)
 
 # Make subsets of data according to the type of data
 
 # Define the variable Generic_Hobbies that will include c("Read", "L.Music", "Cinema", "Comp", "TV", "Gender","Age", "Marital.St", "Prof", "N.Act")
-Generic_Hobbies <- select(hobbies_, c("Read", "L.Music", "Cinema", "Comp", "TV","Gender","Age", "Marital.St", "Prof", "N.Act"))
+Generic_Hobbies <- select(hobbies_, c("Read", "L.Music", "Cinema", "Comp", "TV", "Gender","Age", "Marital.St", "Prof"))
 
 # Define the variable Artistic_Hobbies that will include c("Show", "Exhib", "Play.M", "Collect", "Gender","Age", "Marital.St", "Prof", "N.Act")
-Artistic_Hobbies <- select(hobbies_, c("Show", "Exhib", "Play.M", "Collect", "Gender","Age", "Marital.St", "Prof", "N.Act"))
+Artistic_Hobbies <- select(hobbies_, c("Show", "Exhib", "Play.M", "Collect"))
 
 # Define the variable Sport_Hobbies that will include c("Sport", "Walk" , "Travel", "Fish", "Gender","Age", "Marital.St", "Prof", "N.Act")
 Sport_Hobbies <- select(hobbies_, c("Sport", "Walk" , "Travel", "Fish"))
 
 # Define the variable Handcraft_Hobbies that will include c("Mech", "Garden", "Knitt", "Cook")
-Handcraft_Hobbies <- select(hobbies_, c("Mech", "Garden", "Knitt", "Cook", "Gender","Age", "Marital.St", "Prof", "N.Act"))
+Handcraft_Hobbies <- select(hobbies_, c("Mech", "Garden", "Knitt", "Cook"))
 
 # Define the variable Charity_Hobbies that will include c("Volunteer")
-Charity_Hobbies <- select(hobbies_, c("Volunteer", "Gender","Age", "Marital.St", "Prof", "N.Act"))
+Charity_Hobbies <- select(hobbies_, c("Volunteer"))
 
 #This new categories include 18 out of the 23 variables;
-#Variables like "Gender","Age", "Marital.St", "Prof", "N.Act" are the ones we are using as target variables
+#Variables like "Gender","Age", "Marital.St", "Prof" are the variables defining the individuals so they were included in all the subsets
+#"N.Act" is a quantitative variable
 
 
 #Graphical visualization of the data
 library(GGally)
 library(ggplot2)
 
-pairs(Generic_Hobbies)
+pairs(Generic_Hobbies, mapping = aes(col = Gender, alpha = 0.3), lower = list(combo = wrap("facethist", bins = 20)))
+
+ggpairs(data = Generic_Hobbies, mapping = aes(col = Age, alpha = 0.3), lower = list(combo = wrap("facethist", bins = 20)))
 
 
 
@@ -75,7 +78,7 @@ dimdesc(res.mca)
 plotellipses(res.mca,keepvar=1:4)
 
 
-#Explore how the data pairs with each other 
+#Explore how the data pairs with each other for better development of study hypothesis
 
-#Calculated correlation matrix and visualize it
+#Calculated correlation matrix and visualize it for better development of study hypothesis
 
